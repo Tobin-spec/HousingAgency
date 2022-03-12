fdf7c6f3d4b81b25f8af5bb5118a298d588c0a33
Nedko Nedkov <nn222mx@student.lnu.se>
2022-03-12 06:51:16 +0100

    Checking if database exists and if not, creating it

--- Nedko_Nedkov_assign_two.py
+++ Nedko_Nedkov_assign_two.py
@@ -1,5 +1,22 @@
 import mysql.connector
 
+def ensureCreated(db_name, my_cursor):
+    my_cursor.execute("SHOW DATABASES")  # Get all databases
+    database_exists = False 
+    for db in my_cursor:  # See if dbName is in all databases
+        if db[0] == dbName.lower():
+            database_exists = True
+            break
+    return database_exists
+
+
+def createDatabase(db_name, my_cursor):
+    my_cursor.execute("CREATE DATABASE " + db_name)  # Creates the database
+    print('Successfully created database [' + db_name + ']')
+    my_cursor.execute("USE " + db_name)  # Starts using the database
+    print('Using database [' + db_name + ']')
+
+
 
 cnx = mysql.connector.connect(user = 'root',
                               password = 'root',
@@ -9,3 +26,6 @@ cnx = mysql.connector.connect(user = 'root',
 myCursor = cnx.cursor()
 
 dbName = 'ProgrammingAssignTwo'
+
+if ensureCreated(dbName, myCursor) is False:
+    createDatabase(dbName, myCursor) 
