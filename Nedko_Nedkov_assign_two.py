ef8bd252b5c7c6132d2162cf24ce1be30fd68b3f
Nedko Nedkov <nn222mx@student.lnu.se>
2022-03-12 06:52:36 +0100

    Creating tables

--- Nedko_Nedkov_assign_two.py
+++ Nedko_Nedkov_assign_two.py
@@ -17,15 +17,81 @@ def createDatabase(db_name, my_cursor):
     print('Using database [' + db_name + ']')
 
 
+def createTables(myCursor):
+    print('Creating tables:')
+
+    myCursor.execute("CREATE TABLE Customers(\
+	customerID CHAR(36) NOT NULL,\
+	first_name VARCHAR(20) NOT NULL,\
+	last_name VARCHAR(20) NOT NULL,\
+	personal_number CHAR(13) NOT NULL,\
+	email VARCHAR(50) NOT NULL,\
+	password CHAR(64) NOT NULL,\
+	move_in_date DATE,\
+	move_out_date DATE,\
+	last_rent_paid DATE,\
+	points SMALLINT UNSIGNED NOT NULL,\
+	UNIQUE(personal_number),\
+	PRIMARY KEY (customerID));")
+    print('\t-Sucessfully created table [customers]')
+    
+    myCursor.execute("CREATE TABLE Employees(\
+	employeeID CHAR(36) NOT NULL,\
+	first_name VARCHAR(20) NOT NULL,\
+	last_name VARCHAR(20) NOT NULL,\
+	personal_number CHAR(13) NOT NULL,\
+	phone_number VARCHAR(13) NOT NULL,\
+	email VARCHAR(50) NOT NULL,\
+	hire_date DATE NOT NULL,\
+	release_date DATE,\
+	salary SMALLINT UNSIGNED NOT NULL,\
+	UNIQUE (personal_number),\
+	PRIMARY KEY (employeeID));")
+    print('\t-Successfully created table [employees]')
+
+    myCursor.execute("CREATE TABLE Properties(\
+	propertyID CHAR(36) NOT NULL,\
+	address VARCHAR(200) NOT NULL,\
+	rooms TINYINT NOT NULL,\
+	size DOUBLE NOT NULL,\
+	furnitured BOOL NOT NULL,\
+	customerID CHAR(36),\
+	rent SMALLINT UNSIGNED NOT NULL,\
+	description VARCHAR(1000) NOT NULL,\
+	employeeID CHAR(36) NOT NULL,\
+	PRIMARY KEY(propertyID),\
+	FOREIGN KEY (employeeID) REFERENCES Employees(employeeID));")
+    print('\t-Successfully created table [properties]')
+
+    myCursor.execute("CREATE TABLE Listings(\
+	listingID CHAR(36) NOT NULL,\
+	propertyID CHAR(36) NOT NULL,\
+	publish_date DATE NOT NULL,\
+	last_date DATE NOT NULL,\
+	available_from DATE NOT NULL,\
+	PRIMARY KEY(listingID),\
+	FOREIGN KEY (propertyID) REFERENCES Properties(propertyID));")
+    print('\t-Successfully created table [listings]')
+
+    myCursor.execute("CREATE TABLE Listing_Applications(\
+	listingID CHAR(36) NOT NULL,\
+	customerID CHAR(36) NOT NULL,\
+	PRIMARY KEY(ListingID, CustomerId),\
+	FOREIGN KEY (listingID) REFERENCES Listings(listingID),\
+	FOREIGN KEY (customerID) REFERENCES Customers(customerID));")
+    print('\t-Successfully created table [listing_applications]')
+
+
 
 cnx = mysql.connector.connect(user = 'root',
                               password = 'root',
                               host = 'localhost'
                               )
 
-myCursor = cnx.cursor()
+my_cursor = cnx.cursor()
 
-dbName = 'ProgrammingAssignTwo'
+db_name = 'ProgrammingAssignTwo'
 
-if ensureCreated(dbName, myCursor) is False:
-    createDatabase(dbName, myCursor) 
+if ensureCreated(db_name, my_cursor) is False:
+    createDatabase(db_name, my_cursor) 
+    createTables(my_cursor) 
