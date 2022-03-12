9444725f8bc15c459a2fcab91f8d5a6747564fab
Nedko Nedkov <nn222mx@student.lnu.se>
2022-03-12 07:01:33 +0100

    Adding user input and functionality

--- Nedko_Nedkov_assign_two.py
+++ Nedko_Nedkov_assign_two.py
@@ -1,15 +1,28 @@
-import csv
-import datetime
+from datetime import datetime
 import mysql.connector
+import csv
+import msvcrt as m
+
+
+cnx = mysql.connector.connect(user = 'root',
+                              password = 'root',
+                              host = 'localhost'
+                              )
+
+
+my_cursor = cnx.cursor()
+
+db_name = 'ProgrammingAssignTwo'
+
 
 def ensureCreated(db_name, my_cursor):
     my_cursor.execute("SHOW DATABASES")  # Get all databases
-    database_exists = False 
+    databaseExists = False 
     for db in my_cursor:  # See if db_name is in all databases
         if db[0] == db_name.lower():
-            database_exists = True
+            databaseExists = True
             break
-    return database_exists
+    return databaseExists
 
 
 def createDatabase(db_name, my_cursor):
@@ -299,16 +312,122 @@ def createViews(my_cursor):
                                         # Select, aggregate functions, where, subquery, join,
                                         # group by, order by and limit all in a view
 
+def waitKeyPress():
+    m.getch()  
 
 
-cnx = mysql.connector.connect(user = 'root',
-                              password = 'root',
-                              host = 'localhost'
-                              )
+def getTenantAddresses(my_cursor):
+    my_cursor.execute("SELECT * FROM tenant_addresses;")
+    
+    myresult = my_cursor.fetchall()  
+    print('\n\n================ Tenants ================')
+    for x in myresult:  
+        print(x[0], x[1] + ', ' + x[2])
 
-my_cursor = cnx.cursor()
 
-db_name = 'ProgrammingAssignTwo'
+    print('\nPrinted rows: ' + str(len(myresult)) + '\n=========================================\n\nPress any key to continue...\n')
+    waitKeyPress()
+
+
+def getEmployeesWithLeavingTenants(my_cursor):
+    my_cursor.execute("SELECT * FROM employees_losing_tenants;")
+    myresult = my_cursor.fetchall() 
+    print('\n\n================ Tenant-losing employees ================')
+    for x in myresult:
+        print(x[0], x[1])
+
+    print('\nPrinted rows: ' + str(len(myresult)) + '\n=========================================\n\nPress any key to continue...\n')
+    waitKeyPress()
+
+
+def getAveragePointsByMoveInMonth(my_cursor):
+    my_cursor.execute("SELECT * FROM month_avg_points;")  
+    myresult = my_cursor.fetchall() 
+    print('\n\n================ Average points by move-in month ================')
+    for x in myresult:
+        print(x[0], '(', x[1], ')')
+
+    print('\nPrinted rows: ' + str(len(myresult)) + '\n=========================================\n\nPress any key to continue...\n')
+    waitKeyPress()
+
+
+def getMostInterestedCustomer(my_cursor):
+    my_cursor.execute("SELECT * FROM most_interested_customer;") 
+    myresult = my_cursor.fetchall()  
+    print('\n\n================ Most interested customer ================')
+    for x in myresult:
+        print(x[0], x[1], '(', x[2], ')')
+
+    print('\nPrinted rows: ' + str(len(myresult)) + '\n=========================================\n\nPress any key to continue...\n')
+    waitKeyPress()
+
+
+def getMostWantedProperty(my_cursor):
+    my_cursor.execute("SELECT * FROM most_wanted_property;")
+    myresult = my_cursor.fetchall()
+    print('\n\n================ Most wanted property ================')
+    for x in myresult:
+        print(x[0], '(', x[1], ')')
+
+    print('\nPrinted rows: ' + str(len(myresult)) + '\n=========================================\n\nPress any key to continue...\n')
+    waitKeyPress()
+
+
+def getEmployeeByAddress(my_cursor):
+    address = input("Select property address: ")
+    myresult = my_cursor.callproc('get_employee_in_address', [address, 0, 0])
+
+    print('\n\n================ Employee working at ' + address + ' ================')
+    print(myresult[1], myresult[2])
+
+
+    print('\nPrinted rows: 1\n=========================================\n\nPress any key to continue...\n')
+    waitKeyPress()
+
+
+def getEmployeeByTenant(my_cursor):
+    tenant_personal_number = input('Select tenant personal number: ')
+    myresult = my_cursor.callproc('get_employee_by_tenant', [tenant_personal_number, 0, 0])
+
+
+    print('\n\n================ Employee supervising tenant ' + tenant_personal_number + ' ================')
+    print(myresult[1], myresult[2])
+
+    print('\nPrinted rows: 1\n=========================================\n\nPress any key to continue...\n')
+    waitKeyPress()
+
+
+def getAverageRentForAvailableProperties(my_cursor):
+    my_cursor.execute("SELECT * FROM available_properties_avg_rent;")
+    myresult = my_cursor.fetchall() 
+    print('\n\n================ Available properties AVG rent ================')
+    for x in myresult: 
+        print(x[0])
+
+    print('\nPrinted rows: ' + str(len(myresult)) + '\n=========================================\n\nPress any key to continue...\n')
+    waitKeyPress()
+
+
+def getAveragePointsByEmployee(my_cursor):
+    my_cursor.execute("SELECT * FROM average_points_by_employee;")  
+    myresult = my_cursor.fetchall()
+    print('\n\n================ Average points by employee ================')
+    for x in myresult:
+        print(x[0], x[1], '(', x[2], ')')
+
+    print('\nPrinted rows: ' + str(len(myresult)) + '\n=========================================\n\nPress any key to continue...\n')
+    waitKeyPress()
+
+
+def getAverageSalaryForWantedEmployees(my_cursor):
+    my_cursor.execute("SELECT * FROM average_salary_wanted_employees;")  
+    myresult = my_cursor.fetchall()
+    print('\n\n================ Average salary for wanted employees ================')
+    for x in myresult:
+        print(x[0])
+    print('\nPrinted rows: ' + str(len(myresult)) + '\n=========================================\n\nPress any key to continue...\n')
+    waitKeyPress()
+
 
 if ensureCreated(db_name, my_cursor) is False:
     createDatabase(db_name, my_cursor) 
@@ -321,3 +440,54 @@ cnx = mysql.connector.connect(user = 'root',
                               host = 'localhost',
                               database = db_name)
 
+while(True):
+    print('============================== Options ==============================\n\
+     1. Get the names and addresses of all customers with a property \n\
+     2. Get the names of all employees, who are responsible for a room, which a tenant is leaving \n\
+     3. Get the average amount of points per move-in month \n\
+     4. Get the customer with most applications for a property \n\
+     5. Get the property with most applications by customers \n\
+     6. Get the name of the employee working in a specific property \n\
+     7. Get the name of the employee responsible for rooms, in which a certain customer lives \n\
+     8. Get the average rent of available properties \n\
+     9. Get the average points of customers by responsible employee \n\
+    10. Get the average salary of employees responsible for the properties the most interested customer wants \n\
+    11. Exit')
+    print('=====================================================================\n')
+
+    option = int(input("Pick an option (1-11): "))
+    my_cursor = cnx.cursor()    
+    if option == 1:
+        getTenantAddresses(my_cursor)
+
+    if option == 2:
+        getEmployeesWithLeavingTenants(my_cursor)
+
+    if option == 3:
+        getAveragePointsByMoveInMonth(my_cursor)
+
+    if option == 4:
+        getMostInterestedCustomer(my_cursor)
+
+    if option == 5:
+        getMostWantedProperty(my_cursor)
+
+    if option == 6:
+        getEmployeeByAddress(my_cursor)
+
+    if option == 7:
+        getEmployeeByTenant(my_cursor)
+
+    if option == 8:
+        getAverageRentForAvailableProperties(my_cursor)
+
+    if option == 9:
+        getAveragePointsByEmployee(my_cursor)
+
+    if option == 10:
+        getAverageSalaryForWantedEmployees(my_cursor)
+    
+    if option == 11:
+        break
+    
+    my_cursor.close()
